93bdf6c2c83b79cca2389fc8798dedc7c1ff22b5
Edward Ruzek <eruzek@teamibr.com>
2024-09-21 10:39:04 -0400

    adding webgl rendering and vertex randomization

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"math/rand/v2"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,7 +15,34 @@ func main() {
 	r.StaticFile("", "./ui/index.html")
 
 	// serve javascript webgl
-	r.StaticFile("/js/webgl.js", "./src/js/webgl.js")
+	r.StaticFile("/js/webgl.js", "./src/js/webgl.js"))
 
+	// api
+	r.GET("/api/verticies", GetVerticies)
 	r.Run("localhost:8080")
+}
+
+func GetVerticies(c *gin.Context) {
+	numVerticies, err := strconv.Atoi(c.Query("numVerticies"))
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	verticies := generateVerticies(numVerticies)
+	c.JSON(http.StatusOK, gin.H{
+		"numVerticies": numVerticies,
+		"verticies":    verticies,
+	})
+}
+
+func generateVerticies(numVerticies int) []float32 {
+	verticies := make([]float32, 3*numVerticies)
+
+	for i := 0; i < numVerticies; i++ {
+		verticies[3*i] = rand.Float32()*10 - 5
+		verticies[3*i+1] = rand.Float32()*10 - 5
+		verticies[3*i+2] = rand.Float32()*10 - 5
+	}
+	return verticies
 }
--- src/js/webgl.js
+++ src/js/webgl.js
@@ -1,23 +1,200 @@
+const vertexShaderSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        gl_PointSize = 1.0;
+    }
+`;
+
+const fragmentShaderSource = `
+    void main() {
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+`;
+
+var positions = [];
+
 main();
 
+document.querySelector("button").onclick = buttonClickHandler.bind(document);
+
+function buttonClickHandler() {
+    console.log(document);
+    getVerticies(document.querySelector("#numVerticies").value);
+}
 //
 // start here
 //
 function main() {
-  const canvas = document.querySelector("#glViewport");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+    const canvas = document.querySelector("#glViewport");
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl");
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.",);
+        return;
+    }
+
+    const shaderProgram = initShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
+
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+        },
+    }
+
+    setInterval(() => {
+        // Set clear color to black, fully opaque
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        // Clear the color buffer with specified clear color
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    
+        const buffers = initBuffers(gl, positions);
+    
+        // Draw the scene
+        drawScene(gl, programInfo, buffers);
+    }, 1000)
+}
+
+function initShaderProgram(gl, vShaderSource, fShaderSource) {
+    // load shaders
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vShaderSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fShaderSource);
+
+    // create shader program
+    const shaderProgram = gl.createProgram();
+    // attach shaders
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // alert program creation errors
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+function initBuffers(gl, verticies) {
+    const positionBuffer = initPositionBuffer(gl, verticies);
+    return {
+        position: positionBuffer,
+    };
+}
+
+function initPositionBuffer(gl, verticies) {
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW);
+
+    return positionBuffer;
+}
+
+function drawScene(gl, programInfo, buffers) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    const modelViewMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset,
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+    }
+
+    gl.useProgram(programInfo.program);
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix,
     );
-    return;
-  }
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix,
+    );
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    {
+        const offset = 0;
+        const vertexCount = positions.length;
+        gl.drawArrays(gl.POINTS, offset, vertexCount);
+    }
+}
+
+
+
+function generateVerticies(num) {
+    var verticies = [];
+    for (var i = 0; i < num; i++) {
+        do {
+            x = Math.random()*4-2;
+            y = Math.pow(x, Math.random()*5-2.5) * (Math.random()*8-4)
+        } while (isNaN(x) || isNaN(y))
+        verticies.push(x);
+        verticies.push(y);
+    }
+    return verticies;
+}
+
+function getVerticies(numVerticies) {
+    fetch(`http://localhost:8080/api/verticies?numVerticies=${numVerticies}`).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        positions = data.verticies; 
+    });
 }
