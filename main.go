96bbaf706791c545403bab5c8beb40c83dee2ab6
Edward Ruzek <eruzek@teamibr.com>
2024-09-29 07:43:31 -0400

    adding xml marshaling and experimenting with the go profiler

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"encoding/xml"
+	"io"
 	"math/rand/v2"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/pprof"
 )
+
+type Phone struct {
+	Width   float32 `xml:"width"`
+	Length  float32 `xml:"length"`
+	Height  float32 `xml:"height"`
+	Speaker Speaker `xml:"Speaker"`
+}
+
+type Speaker struct {
+	Width  float32 `xml:"width"`
+	Height float32 `xml:"height"`
+	Center float32 `xml:"center"`
+}
+
+func parseXml(xmlFile string) (*Phone, error) {
+	f, err := os.Open(xmlFile)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	byteValue, err := io.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var phone Phone
+	xml.Unmarshal(byteValue, &phone)
+
+	return &phone, err
+}
 
 func main() {
 	r := gin.Default()
@@ -19,7 +55,27 @@ func main() {
 
 	// api
 	r.GET("/api/verticies", GetVerticies)
+	r.GET("/api/phone", GetPhoneDimensions)
+
+	// profiler registrations
+	pprof.Register(r)
+
+	// run web server
 	r.Run("localhost:8080")
+}
+
+func GetPhoneDimensions(c *gin.Context) {
+	model := c.Query("model")
+
+	var filepath = model + ".xml"
+
+	phone, err := parseXml(filepath)
+	if err != nil {
+		c.AbortWithStatus(http.StatusTeapot)
+		return
+	}
+
+	c.JSON(http.StatusOK, phone)
 }
 
 func GetVerticies(c *gin.Context) {
